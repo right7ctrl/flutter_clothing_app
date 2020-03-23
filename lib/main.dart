65acbaf0f1f7e5a319da0dc13e03576de2000539
right7ctrl <murataslanx33@gmail.com>
2020-03-23 21:57:37 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:clothing_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
